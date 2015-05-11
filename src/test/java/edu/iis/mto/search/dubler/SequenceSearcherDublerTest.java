8f09cb98dadb5a73b39d15006c28b7d665988d03
Arkadiusz Stępniewski <arekstepek@gmail.com>
2015-05-11 13:06:10 +0200

    TestCase for key is not in sequence should return false

--- src/test/java/edu/iis/mto/search/dubler/SequenceSearcherDublerTest.java
+++ src/test/java/edu/iis/mto/search/dubler/SequenceSearcherDublerTest.java
@@ -19,4 +19,15 @@ public class SequenceSearcherDublerTest {
 		assertThat(sequenceSearcher.search(key, seq).isFound(), is(expected));
 	}
 
+	@Test
+	public void search_keyIsNotInSequence_shouldReturnFalse() {
+
+		final int[] seq = new int[] { 1, 10, 20, 50, 100 };
+		final int key = 25;
+		final boolean expected = false;
+		
+		SequenceSearcherDubler sequenceSearcher = new SequenceSearcherDubler();
+
+		assertThat(sequenceSearcher.search(key, seq).isFound(), is(expected));
+	}
 }
