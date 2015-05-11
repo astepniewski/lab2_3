c3139cfd592cb415b8e4d5e015a76869291bb357
Arkadiusz Stępniewski <arekstepek@gmail.com>
2015-05-11 13:50:36 +0200

    TestCase calculateJackardSimilarity with many Elements And Zero Length Sequences should Return 0.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -64,5 +64,18 @@ public class SimilarityFinderTest {
 		assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2),
 				is(expected));
 	}
+	
+	@Test
+	public void calculateJackardSimilarity_manyElementsAndZeroLengthSequences_shouldReturn0() {
+
+		final int[] seq1 = new int[] { 1, 10, 20, 50, 100 };
+		final int[] seq2 = new int[0];
+
+		final double expected = 0.0;
+
+		assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2),
+				is(expected));
+
+	}
 
 }
